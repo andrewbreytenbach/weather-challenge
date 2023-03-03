fb02ca5dfbb94963ed61c819516eb0753d9b73c5
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-03-03 15:24:12 -0500

    Using the OpenWeatherMap APIs, we retrieve geographical coordinates given a city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 const apikey = '84b1f784083597760ab80c6e7e75720c'; // Replace YOUR_API_KEY with your actual API key from OpenWeatherMap
 const city = ''; // The name of the city you want to get the weather forecast for
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`;
+// Using the OpenWeatherMap APIs, we retrieve geographical coordinates given a city name
+const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apikey}`;
 
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-    console.log(data.list); // This will log an array of objects representing the weather forecast for the next 5 days
+    const lat = data[0].lat; // This will retrieve the latitude coordinate for the specified city
+    const lon = data[0].lon; // This will retrieve the longitude coordinate for the specified city
+    console.log(`Latitude: ${lat}, Longitude: ${lon}`);
   })
   .catch(error => console.log(error));
