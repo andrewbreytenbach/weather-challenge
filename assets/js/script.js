9b57277a5d1c74ede5d6e18a83cec8a475fa7dfc
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-03-06 14:38:23 -0500

    added new js code to begin building out the gerweatherforecast function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
-const apikey = '84b1f784083597760ab80c6e7e75720c'; // Replace YOUR_API_KEY with your actual API key from OpenWeatherMap
+// This sets a variable using my API key that can then be plugged into the url later.
+const apikey = '84b1f784083597760ab80c6e7e75720c'; 
 
+// This is the function the html file will call when the submit button is clicked to get the weather forecast for the searched city.
+function getWeatherForecast() {
+    // This sets the city variable equivalent to whatever city was searched for.
+    const city = document.getElementById('city').value;
+    // This is the weather api for the 5 day forecast
+    const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apikey}`;
+}
