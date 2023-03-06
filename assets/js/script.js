a5306c02b1542049697507c4f21b98d0724a87cd
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-03-06 14:43:05 -0500

    uses a basic fetch function to use the 5 day forecast api previously declared

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,18 @@ function getWeatherForecast() {
     const city = document.getElementById('city').value;
     // This is the weather api for the 5 day forecast
     const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apikey}`;
+
+    // This uses a basic fetch function to use the 5 day forecast api previously declared
+    fetch(geoUrl)
+        // This is basic syntax for a fetch function 
+        .then(response => response.json())
+        .then(data => {
+            // This uses an API that gets the latitude and longitude of the city put into the search
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+            const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apikey}`;
+            
+            // This returns the forecast url of the specified city with its latitude and longitudinal coordinates
+            return fetch(forecastUrl);
+            })
 }
