e1aee509b428571ca50388774cd1cce5ed25d15d
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-03-07 13:52:51 -0500

    made some changes to the js so it fixes the functions and makes them display the 5 day weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const apikey = '84b1f784083597760ab80c6e7e75720c';
 
 function getWeatherForecast() {
     // This sets the city variable equivalent to whatever city was searched for.
-    const city = document.getElementById('city').value;
+    const city = document.getElementById('city-input').value;
     // This is the weather api for the 5 day forecast
     const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apikey}`;
   
@@ -26,7 +26,7 @@ function getWeatherForecast() {
         // This declares a variable that will then be used to display the forecast to the html
         const forecastDiv = document.getElementById('forecast');
         // This clears any existing content in the html
-        forecastDiv.innerHTML = ''; 
+        forecastDiv.innerHTML = '';
   
         // Use a for loop to display the 5-day forecast
         for (let i = 0; i < data.list.length; i++) {
@@ -40,112 +40,113 @@ function getWeatherForecast() {
           // Create a div element for each forecast item
           const forecastItemDiv = document.createElement('div');
           forecastItemDiv.classList.add('list-group', 'text-center');
-
+  
           // Add the date to the forecast item
           const dateDiv = document.createElement('div');
           const forecastDate = date.toLocaleDateString('en-US', {weekday: 'short'});
           dateDiv.textContent = forecastDate;
           forecastItemDiv.appendChild(dateDiv);
-          
-          // Add the weather icon to the forecast item
-          const iconImg = document.createElement('img');
-          iconImg.src = iconUrl;
-          forecastItemDiv.appendChild(iconImg);
-          
-          // Add the temperature to the forecast item
-          const tempDiv = document.createElement('div');
-          tempDiv.textContent = '${temp}°F';
-          forecastItemDiv.appendChild(tempDiv);
-          
-          // Add the wind speed to the forecast item
-          const windDiv = document.createElement('div');
-          windDiv.textContent = 'Wind: ${windSpeed} mph';
-          forecastItemDiv.appendChild(windDiv);
-          
-          // Add the humidity to the forecast item
-          const humidityDiv = document.createElement('div');
-          humidityDiv.textContent = 'Humidity: ${humidity}%';
-          forecastItemDiv.appendChild(humidityDiv);
-          
-          // Add the forecast item to the forecast div
-          const forecastDiv = document.getElementById('forecast');
-          forecastDiv.appendChild(forecastItemDiv);
-          }
-          
-          // Save the searched city to local storage and add it to the search history
-          addSearchedCityToHistory(city);
-          })
-          // This logs error to the console log if any of the previous fetch functions do not work.
-          .catch(error => console.log(error));
-          }
-          
-          // Get search history from local storage and display it on the page
-          function displaySearchHistory() {
-          const searchHistory = getSearchHistoryFromLocalStorage();
-          if (searchHistory && searchHistory.length > 0) {
-          const searchHistoryDiv = document.getElementById('search-history');
-          const searchHistoryList = document.createElement('ul');
-          searchHistoryList.classList.add('search-history-list');
-          for (let i = 0; i < searchHistory.length; i++) {
-          const searchItem = document.createElement('li');
-          searchItem.textContent = searchHistory[i];
-          searchItem.classList.add('search-history-item');
-          searchItem.addEventListener('click', () => getWeatherForecast(searchHistory[i]));
-          searchHistoryList.appendChild(searchItem);
-          }
-          searchHistoryDiv.appendChild(searchHistoryList);
-          }
-          }
-          
-          displaySearchHistory();
-          
-          // Save search history to local storage
-          function saveSearchHistoryToLocalStorage(searchHistory) {
-          localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-          }
-          
-          // Add searched city to search history
-          function addSearchedCityToHistory(city) {
-          let searchHistory = getSearchHistoryFromLocalStorage();
-          if (!searchHistory) {
-          searchHistory = [];
-          }
-          if (!searchHistory.includes(city)) {
-          searchHistory.unshift(city);
-          if (searchHistory.length > 10) {
-          searchHistory.pop();
-          }
-          saveSearchHistoryToLocalStorage(searchHistory);
-          }
-          displaySearchHistory();
-          }
-          
-          // Get search history from local storage
-          function getSearchHistoryFromLocalStorage() {
-          const searchHistory = localStorage.getItem('searchHistory');
-          if (searchHistory) {
-          return JSON.parse(searchHistory);
-          }
-          return null;
-          }
 
-  // Clear the search history from local storage and from the page
+              // Add the weather icon to the forecast item
+    const iconImg = document.createElement('img');
+    iconImg.src = iconUrl;
+    forecastItemDiv.appendChild(iconImg);
+    
+    // Add the temperature to the forecast item
+    const tempDiv = document.createElement('div');
+    tempDiv.textContent = `${temp}°F`;
+    forecastItemDiv.appendChild(tempDiv);
+    
+    // Add the wind speed to the forecast item
+    const windDiv = document.createElement('div');
+    windDiv.textContent = `Wind: ${windSpeed} mph`;
+    forecastItemDiv.appendChild(windDiv);
+    
+    // Add the humidity to the forecast item
+    const humidityDiv = document.createElement('div');
+    humidityDiv.textContent = `Humidity: ${humidity}%`;
+    forecastItemDiv.appendChild(humidityDiv);
+    
+    // Add the forecast item to the forecast div
+    const forecastDiv = document.getElementById('forecast');
+    forecastDiv.appendChild(forecastItemDiv);
+  }
+  
+  // Save the searched city to local storage and add it to the search history
+  addSearchedCityToHistory(city);
+})
+// This logs error to the console log if any of the previous fetch functions do not work.
+.catch(error => console.log(error));
+
+
+}
+
+// Get search history from local storage and display it on the page
+function displaySearchHistory() {
+const searchHistory = getSearchHistoryFromLocalStorage();
+if (searchHistory && searchHistory.length > 0) {
+const searchHistoryDiv = document.getElementById('search-history');
+const searchHistoryList = document.createElement('ul');
+searchHistoryList.classList.add('search-history-list');
+for (let i = 0; i < searchHistory.length; i++) {
+const searchItem = document.createElement('li');
+searchItem.textContent = searchHistory[i];
+searchItem.classList.add('search-history-item');
+searchItem.addEventListener('click', () => getWeatherForecast(searchHistory[i].trim()));
+searchHistoryList.appendChild(searchItem);
+}
+searchHistoryDiv.appendChild(searchHistoryList);
+}
+}
+
+displaySearchHistory();
+
+// Save search history to local storage
+function saveSearchHistoryToLocalStorage(searchHistory) {
+localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+}
+
+// Add searched city to search history
+function addSearchedCityToHistory(city) {
+let searchHistory = getSearchHistoryFromLocalStorage();
+if (!searchHistory) {
+searchHistory = [];
+}
+if (!searchHistory.includes(city)) {
+searchHistory.unshift(city);
+if (searchHistory.length > 10) {
+searchHistory.pop();
+}
+saveSearchHistoryToLocalStorage(searchHistory);
+}
+displaySearchHistory();
+}
+
+// Get search history from local storage
+function getSearchHistoryFromLocalStorage() {
+const searchHistory = localStorage.getItem('searchHistory');
+if (searchHistory) {
+return JSON.parse(searchHistory);
+}
+return null;
+}
+
+// Clear the search history from local storage and from the page
 function clearSearchHistory() {
-    localStorage.removeItem('searchHistory');
-    const searchHistoryDiv = document.getElementById('search-history');
-    searchHistoryDiv.innerHTML = '';
-    }
-    
-    // Set up event listeners
-    const form = document.getElementById('search-form');
-    const clearButton = document.getElementById('clear-button');
-    
-    // Call getWeatherForecast function when the form is submitted
-    form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    getWeatherForecast();
-    });
-    
-    // Call clearSearchHistory function when the clear button is clicked
-    clearButton.addEventListener('click', clearSearchHistory);
-    
+localStorage.removeItem('searchHistory');
+const searchHistoryDiv = document.getElementById('search-history');
+searchHistoryDiv.innerHTML = '';
+}
+
+// Set up event listeners
+const form = document.getElementById('search-form');
+const clearButton = document.getElementById('clear-button');
+
+// Call getWeatherForecast function when the form is submitted
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+getWeatherForecast();
+});
+
+// Call clearSearchHistory function when the clear button is clicked
+clearButton.addEventListener('click', clearSearchHistory);
