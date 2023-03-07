0ca64dcb1007b64bbdbb2b56225bbbe693877030
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-03-07 10:50:12 -0500

    fixed the issue with the weather icones not working appropriately

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,54 +55,50 @@ function getWeatherForecast() {
       // Add the large card to the forecastDiv
       forecastDiv.appendChild(largeCardDiv);
 
-      // Use a for loop to display the 5-day forecast
-      for (let i = 0; i < data.list.length; i++) {
-        const forecast = data.list.filter(item => item.dt_txt.includes('12:00:00'))[i];
-        const date = new Date(forecast.dt * 1000);
-        const temp = Math.round(forecast.main.temp);
-        const description = forecast.weather[0].description;
-        const humidity = forecast.main.humidity;
-        const windSpeed = forecast.wind.speed
-        const iconUrl = 'https://openweathermap.org/img/w/${forecast.weather[0].icon}.png';
-
-        // Create a div element for each forecast item
-        const forecastItemDiv = document.createElement('div');
-        forecastItemDiv.classList.add('forecast-item');
-    
-        // Add the date to the forecast item
-        const dateDiv = document.createElement('div');
-        dateDiv.textContent = date.toLocaleDateString();
-        forecastItemDiv.appendChild(dateDiv);
-    
-        // Add the weather icon to the forecast item
-        const iconImg = document.createElement('img');
-        iconImg.src = iconUrl;
-        forecastItemDiv.appendChild(iconImg);
-    
-        // Add the temperature to the forecast item
-        const tempDiv = document.createElement('div');
-        tempDiv.textContent = `${temp}°F`;
-        forecastItemDiv.appendChild(tempDiv);
-    
-        // Add the wind speed to the forecast item
-        const windDiv = document.createElement('div');
-        windDiv.textContent = `Wind: ${windSpeed} mph`;
-        forecastItemDiv.appendChild(windDiv);
-    
-        // Add the humidity to the forecast item
-        const humidityDiv = document.createElement('div');
-        humidityDiv.textContent = `Humidity: ${humidity}%`;
-        forecastItemDiv.appendChild(humidityDiv);
-    
-        // Add the weather description to the forecast item
-        const descriptionDiv = document.createElement('div');
-        descriptionDiv.textContent = description;
-        forecastItemDiv.appendChild(descriptionDiv);
-    
-        // Add the forecast item to the forecast div
-        const forecastDiv = document.getElementById('forecast');
-        forecastDiv.appendChild(forecastItemDiv);
-      }
+    // Use a for loop to display the 5-day forecast
+for (let i = 0; i < data.list.length; i++) {
+    const forecast = data.list.filter(item => item.dt_txt.includes('12:00:00'))[i];
+    const date = new Date(forecast.dt * 1000);
+    const temp = Math.round(forecast.main.temp);
+    const description = forecast.weather[0].description;
+    const humidity = forecast.main.humidity;
+    const windSpeed = forecast.wind.speed;
+    const iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+  
+    // Create a div element for each forecast item
+    const forecastItemDiv = document.createElement('div');
+    forecastItemDiv.classList.add('forecast-item');
+  
+    // Add the date to the forecast item
+    const dateDiv = document.createElement('div');
+    dateDiv.textContent = date.toLocaleDateString();
+    forecastItemDiv.appendChild(dateDiv);
+  
+    // Add the weather icon to the forecast item
+    const iconImg = document.createElement('img');
+    iconImg.src = iconUrl;
+    forecastItemDiv.appendChild(iconImg);
+  
+    // Add the temperature to the forecast item
+    const tempDiv = document.createElement('div');
+    tempDiv.textContent = `${temp}°F`;
+    forecastItemDiv.appendChild(tempDiv);
+  
+    // Add the wind speed to the forecast item
+    const windDiv = document.createElement('div');
+    windDiv.textContent = `Wind: ${windSpeed} mph`;
+    forecastItemDiv.appendChild(windDiv);
+  
+    // Add the humidity to the forecast item
+    const humidityDiv = document.createElement('div');
+    humidityDiv.textContent = `Humidity: ${humidity}%`;
+    forecastItemDiv.appendChild(humidityDiv);
+  
+    // Add the forecast item to the forecast div
+    const forecastDiv = document.getElementById('forecast');
+    forecastDiv.appendChild(forecastItemDiv);
+  }
+  
     })
     // This logs error to the console log if any of the previous fetch functions do not work.
     .catch(error => console.log(error));
